4fe31d7f9d61ee514b8e38bdbd126efd01bef416
mrjohn69wick <mr.john69wick@gmail.com>
2026-02-04 11:46:54 -0800

    Fix useHashLocation import in main.jsx

--- trading-book-fixed/src/main.jsx
+++ trading-book-fixed/src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Router } from 'wouter'
-import useHashLocation from 'wouter/use-hash-location'
+
+
+import { useHashLocation } from 'wouter/use-hash-location'
 import './index.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
